9befc0a4baba119c3b4b9d2b116ce3da0f5e6f90
Kate Beard <asouthernbellein@gmail.com>
2018-08-23 11:09:45 +0100

    Finish initial form styles

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,12 +5,12 @@ const router = express.Router();
 //import route controllers
 const home = require('./home');
 const login = require('./login');
+// const signup = require('./signup');
 const error = require('./error.js');
 
 // add routes
 router.get('/', home.get);
 router.get('/login', login.get);
-//router.post('/auth', auth.post);
 router.use(error.client);
 router.use(error.server);
 
