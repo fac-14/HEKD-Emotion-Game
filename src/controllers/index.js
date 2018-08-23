b69efd0d2f5d35b0c682141b61c6b6d77f270bca
Dominic Coelho <domwork@live.com>
2018-08-23 11:03:30 +0100

    signup routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,12 +5,15 @@ const router = express.Router();
 //import route controllers
 const home = require('./home');
 const login = require('./login');
+const signup = require('./signup')
 const error = require('./error.js');
 
 // add routes
 router.get('/', home.get);
 router.get('/login', login.get);
 //router.post('/auth', auth.post);
+router.get('/signup', signup.get)
+// router.post('/signup', signup.post)
 router.use(error.client);
 router.use(error.server);
 
